6496956e650f11e22943e5539ccc1b588c36d832
broline <brolineoronu3@gmail.com>
2022-09-08 17:53:23 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,6 @@
 #include <stdio.h>
 
-/**main -Entry point
- * Description :prints a string
- */
+/* Description :prints a string */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
