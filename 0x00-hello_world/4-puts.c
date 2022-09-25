99676bf83ef6773597340e47394641cfb2e97cd7
broline <brolineoronu3@gmail.com>
2022-09-25 08:13:00 -0700

    added puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 
 /**
-*Description :prints a string 
-*Return: string
-*/
+ * main- prints a string
+ *
+ * Returns: Always 0 (Success)
+ */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
