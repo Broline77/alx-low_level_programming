9a368bed825876ca8812660e97b8a925cd224d14
broline <brolineoronu3@gmail.com>
2022-10-05 23:52:47 -0700

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ *main - prints the phrase putchar plus a new line
+ *
+ * Description: no using standard libraries
+ *Return: 0
+ *
+ */
+int main(void)
+{
+	int a = 95;
+	int b = 112;
+	int c = 117;
+	int d = 116;
+	int e = 99;
+	int f = 104;
+	int g = 97;
+	int h = 114;
+
+	_putchar(a);
+	_putchar(b);
+	_putchar(c);
+	_putchar(d);
+	_putchar(e);
+	_putchar(f);
+	_putchar(g);
+	_putchar(h);
+	_putchar(10);
+
+return (0);
+}
