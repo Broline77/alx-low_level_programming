5f6d132238f14afb6413cc2096bb09e0530a29c6
broline <brolineoronu3@gmail.com>
2022-10-06 17:58:38 -0700

    alphas

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include  "main.h"
+
+/**
+ *main - prints lowercase alhabets and a new line
+ *
+ * Return: 0
+ */
+void print_aphabet(void)
+{
+	int i;
+
+	for (i = 97; i < 123; i++)
+	{
+		_putchar(i);
+	}
+	_putchar(10);
+return (0);
+}
