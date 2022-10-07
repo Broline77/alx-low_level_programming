574c3217616fc8a1c624a3484f15024380ea1698
broline <brolineoronu3@gmail.com>
2022-10-06 19:47:08 -0700

    x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints the aplhabet 10 time in lowecase
+ * followed by a new line
+ * no stdlib
+ *
+ * Return: Allways o
+ */
+void print_alphabet_x10(void)
+{
+	int i = 0;
+	char c;
+
+	while (i < 10)
+	{
+		for (c = 'a'; c < 'z'; c++)
+		{
+			_putchar(c);
+		}
+		_putchar('\n');
+	}
+}
