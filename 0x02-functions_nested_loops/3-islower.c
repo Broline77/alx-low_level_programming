85fc0789631c823e62f724b6f066ba8f3321e60f
broline <brolineoronu3@gmail.com>
2022-10-06 20:47:23 -0700

    1st

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _islower - checks if a character c is lowercase
+ * Return: 1 if lowercase and 0 otherwise
+ */
+int _islower(int c)
+{
+	int i;
+
+	for (i = 'a'; i <= 122; i++)
+	{
+		_putchar("1");
+	}
+return (0);
