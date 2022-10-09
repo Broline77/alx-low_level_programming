7f4d9a01dc9a64453d94208737660a66fcded99d
broline <brolineoronu3@gmail.com>
2022-10-09 07:54:27 -0700

    list of nums

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * print_to_98 - prints nums up to 98 from n
+ *
+ * int n : starting value of the list
+ *
+ * Return: list of nums
+ */
+void print_to_98(int n)
+{
+	for (n = '0'; n <= '9'; n++)
+	{
+		_putchar(n);
+		if (n != '9')
+		{
+			_putchar(',');
+			_putchar(' ');
+		}
+		_putchar('\n');
+	}
+}
+
