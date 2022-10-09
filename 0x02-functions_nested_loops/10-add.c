0a1f3a50dd7750ac8ed1ef76177c481cdbaca23b
broline <brolineoronu3@gmail.com>
2022-10-09 05:31:37 -0700

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * add - function that adds two integers
+ *
+ * assigned integers to be added
+ *
+ * Return: the summed up result
+ */
+int add(int i, int j)
+{
+	int r;
+
+	r = i + j;
+	_putchar(r + '0');
+	return (r);
+}
+
