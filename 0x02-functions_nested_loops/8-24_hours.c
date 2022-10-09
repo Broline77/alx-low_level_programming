db30caa0211c715a7d860c614cd93949cbd1a428
broline <brolineoronu3@gmail.com>
2022-10-08 18:32:36 -0700

    jack

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * jack_bauer - print every minute of the day
+ *
+ * Return: a list
+ *
+ */
+void jack_bauer(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i < 24; i++)
+	{
+	for (j = 0; j < 59; j++)
+
+	_putchar(i + '0');
+	_putchar(':');
+	_putchar('j');
+	}
+}
