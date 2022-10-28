0f7b41f602fde90e8c18117ecb65752679376bbb
broline <brolineoronu3@gmail.com>
2022-10-28 13:55:42 -0700

    puts

---
+++ testcase/hello/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints a string of characters
+ *
+ * Return: Always 0(success)
+ */
+int main(void)
+{
+	puts("\"programming is like building a multilingual puzzle");
+	return (0);
+}
+
