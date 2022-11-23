953d003b3d0a6392b21796f15a752c305561371e
broline <brolineoronu3@gmail.com>
2022-11-22 17:24:00 -0800

    stk

---
+++ testcase/0x02-functions_nested_loops/nl/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _print_last_digit - prints last digit of an int
+ * @n: the int to be passed
+ * Return: the last digit
+ */
+int print_last_digit(int n)
+{
+	if (n < 0)
+	{
+		n = -1;
+	}
+}
+	return (n % 10);
