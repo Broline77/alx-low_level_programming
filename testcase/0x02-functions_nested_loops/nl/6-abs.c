7c1f6bca16edd0ef7091e43f33b3adea8cae5b55
broline <brolineoronu3@gmail.com>
2022-11-22 16:59:17 -0800

    absolute

--- testcase/0x02-functions_nested_loops/nl/6-abs.c
+++ testcase/0x02-functions_nested_loops/nl/6-abs.c
@@ -1,2 +1,20 @@
 #include "main.h"
 
+/**
+ * _abs - computes the absolute value of an int
+ *
+ * @int: aparam to be passed
+ * Return: the absolute valie of an in int
+ *
+ */
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		return (n * -1);
+	}
+	else
+	{
+		return (n);
+	}
+}
